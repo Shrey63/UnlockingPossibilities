67ee7dab6fea37d69a4e239bc1eedb933880d361
Shrey63 <shreyaparasagrawal@gmail.com>
2023-04-14 21:57:37 +0530

    Sign up and sign in for both professor and student completed

--- vocational_course/urls.py
+++ vocational_course/urls.py
@@ -5,7 +5,12 @@ from django.contrib import admin
 from django.urls import path
 from vocational_course import views
 urlpatterns = [
-path('',views.getProffLoginPage),
+path('ProfessorSignIn',views.getProfessorSignIn),
+path('ProfessorSignUp',views.getProfessorSignUp),
+path('StudentSignIn',views.getStudentSignIn),
+path('StudentSignUp',views.getStudentSignUp),
+
+
 # path('LogIn',views.userLogin),
 # path('', views.home),
 path('Logout',views.logout)
